13f6f5ff44e818afb8644b5a0e18c7a0f7b762f9
Jason <colonjasonm@gmail.com>
2026-03-11 21:53:08 -0500

    Added backend call, file upload, landing page

--- epsgui.py
+++ epsgui.py
@@ -4,6 +4,7 @@ import webbrowser
 import tkinter.font as tkFont
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import epsread
 
 bgcolor="grey"
 msgcolor="white"
@@ -13,20 +14,37 @@ graphx=600
 graphy=300
 chcount=8
 chapters=[1, 2, 3, 4, 5, 6, 7, 8, 9]
-chwords=[1204, 750, 251, 2145, 3125, 800, 1902, 750, 2400]
+chwords=[1204, 750, 251, 2145, 3125, 800, 1902, 750, 2400] #sample data
 maxwords=3500
+
+def getfile():
+    filepath = filedialog.askopenfilename(filetypes=[("Epub filetype", "*.epub")])
+    if not filepath:
+        return
+    epsread.getdata(filepath)
+    landframe.pack_forget()
+    dataframe.pack(side="top")
 
 epsgui = tk.Tk(className="E-Pub Stats") #window widget
 epsgui.geometry(f"{esguix}x{esguiy}")
 epsgui.configure(bg=bgcolor)
 
-msgfont = tkFont.Font(family="Calibre", size=16, weight="bold")
+msgfont = tkFont.Font(family="Calibre", size=16, weight="bold") #font settings
 btnfont = tkFont.Font(family="Calibre", size=14)
 
-#top-packed widgets
+landframe = tk.Frame(bg=bgcolor) #file selection frame
+landframe.pack()
 
-wpmframe = tk.Frame(bg=bgcolor, padx=5, pady=20) 
+uploadbtn = tk.Button(landframe, text="Open EPub", width=20, command=getfile)
+uploadbtn.pack()
+
+dataframe = tk.Frame(bg=bgcolor) #graph page frame
+#dataframe.pack(side="top")
+
+wpmframe = tk.Frame(dataframe, bg=bgcolor, padx=5, pady=20) #frame for wpm entry
 wpmframe.pack(side="top")
+
+#top-packed widgets
 
 wpmlabelleft = tk.Label(wpmframe, text="Enter your reading speed: ", fg=msgcolor, font=msgfont)
 wpmlabelleft.grid(row=0, column=0)
@@ -42,14 +60,14 @@ wpmlabelright = tk.Label(wpmframe, text=" words per minute", fg=msgcolor, font=m
 wpmlabelright.grid(row=0, column=2)
 wpmlabelright.configure(bg=bgcolor)
 
-wpmlink = tk.Button(epsgui, text="Don't know your WPM? Click here to test!", width =40, font=btnfont, command=lambda: webbrowser.open("https://swiftread.com/reading-speed-test"))
+wpmlink = tk.Button(dataframe, text="Don't know your WPM? Click here to test!", width =40, font=btnfont, command=lambda: webbrowser.open("https://swiftread.com/reading-speed-test"))
 wpmlink.pack()
 
 #bottom-packed widgets
-closebutton = tk.Button(epsgui, text="Close", width =20, font=btnfont, command=epsgui.destroy)
+closebutton = tk.Button(dataframe, text="Close", width =20, font=btnfont, command=epsgui.destroy)
 closebutton.pack(side="bottom", pady=20)
 
-grframe = tk.Frame(epsgui, bg="lightgrey", width=graphx, height=graphy)
+grframe = tk.Frame(dataframe, bg="lightgrey", width=graphx, height=graphy)
 grframe.pack(side="bottom", padx=20, pady=20)
 
 #graph
@@ -62,4 +80,3 @@ grcanvas.draw()
 grcanvas.get_tk_widget().pack()
 
 epsgui.mainloop()
-
--- epsread.py
+++ epsread.py
@@ -7,40 +7,39 @@ from ebooklib import epub
 import html.parser
 from html.parser import HTMLParser
 
-bookname = "TheAgeOfReason.epub" # finalname
+def getdata(bookpath):
+    book = epub.read_epub(bookpath, options={"ignore_ncx":True}) #sets up epubbook reader
 
-book = epub.read_epub(bookname, options={"ignore_ncx":True}) #sets up epubbook reader
+    class bodyparser(HTMLParser): #create htmlparser class and tags
+        def handle_starttag(self, tag, attrs): #start tag for body parser
+            if tag == "body":
+                # print("open")
+                self.isbody = True
+        def handle_endtag(self, tag): #end tag for body parser
+            if tag == "body":
+                # print("close body")
+                self.isbody = False
+        def handle_data(self, data):
+            if self.isbody == True:
+                self.bodytext.append(data)
 
-class bodyparser(HTMLParser): #create htmlparser class and tags
-    def handle_starttag(self, tag, attrs):
-        if tag == "body":
-            # print("open")
-            self.isbody = True
-    def handle_endtag(self, tag):
-        if tag == "body":
-            # print("close body")
-            self.isbody = False
-    def handle_data(self, data):
-        if self.isbody == True:
-            self.bodytext.append(data)
+    textget = bodyparser() #declare htmlparser
+    textget.bodytext = [] #body text handler
+    textget.isbody = False #body tag boolean
 
-textget = bodyparser() #declare htmlparser
-textget.bodytext = [] #body text handler
-textget.isbody = False #body tag boolean
-
-chnum = 0
-for chapter_id, linear in book.spine: #iterate through chapter list
-    if linear == "yes": #and chapter_id != "titlepage": #get rid of unordered items
-        textget.bodytext = [] #reset parser text
-        testchapter = book.get_item_with_id(chapter_id) #get chapter item
-        textget.feed(testchapter.get_content().decode('utf-8')) #feed the text content of the chapter to the parser
-        #chtext = ' '.join(textget.bodytext)
-        chwords = ' '.join(textget.bodytext).split() #get words by combining html elements and separating words
-        chwc = len(chwords) #get wordcount via length of string array
-        if chwc > 100:
-            chnum += 1
-            print("Chapter",chnum)
-            print("Wordcount:",chwc)
+    chnum = 0
+    for chapter_id, linear in book.spine: #iterate through chapter list
+        if linear == "yes": #and chapter_id != "titlepage": #get rid of unordered items
+            textget.bodytext = [] #reset parser text every chapter
+            testchapter = book.get_item_with_id(chapter_id) #get chapter item
+            textget.feed(testchapter.get_content().decode('utf-8')) #feed the text content of the chapter to the parser
+            chtext = ' '.join(textget.bodytext)
+            chwords = ' '.join(textget.bodytext).split() #get words by combining html elements and separating words
+            chwc = len(chwords) #get wordcount via length of string array
+            if chwc > 100:
+                chnum += 1
+                print("Chapter",chnum)
+                print("Wordcount:",chwc)
 
 #print(testchapter.get_content().decode('utf-8'))
 #print(textget.bodytext)
